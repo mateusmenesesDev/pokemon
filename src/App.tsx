0aed3d6d36efc1afacd2b74cdae5884d92a829a9
Mateus Meneses <mateusppa10@gmail.com>
2022-10-18 11:20:23 -0300

    Conseguindo puxar pokemon via PokeApi e utilizando melhores imagens via HybridShivam github. Projeto necessitando de organização de componentes e estilizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+interface Pokemon {
+  name:'',
+  id:number
+  types:[{type:{name:''}}],
+  sprites:{front_default:''}
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[pokemon, setPokemon] = useState<Pokemon|null>(null)
+  const pokemonApi = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2/pokemon/'
+  })
+
+  const getPokemon = async ()=>{
+    const response = await pokemonApi.get('meganium')
+    const pokemon:Pokemon = response.data
+    setPokemon(pokemon)    
+  }
+  const basePokemonImage = `https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${String(pokemon?.id).padStart(3,'0')}.png`
+  console.log(basePokemonImage)
+  useEffect(()=>{
+    getPokemon()
+  },[])
+    return (
+    <div className="h-[100vh] bg-gradient-to-r from-purple-500 to-pink-500 flex justify-center items-center">
+      {pokemon && <div className="">
+        <img src={basePokemonImage} alt="" className="w-[300px]"/>
+        <h3>{pokemon.name}</h3>
+        {pokemon.types.map(pokemonType=>(<h4>{pokemonType.type.name}</h4>))}
+        <p>Description</p>
+      </div>}
     </div>
   );
 }
