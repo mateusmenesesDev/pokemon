8a534981661631a7daf96a40d5c86fe575fa7a12
Mateus Meneses <mateusppa10@gmail.com>
2022-10-18 20:46:24 -0300

    :lipstick: Resolvendo bugs de estilo 2

--- src/components/Pokemon/Stats/index.tsx
+++ src/components/Pokemon/Stats/index.tsx
@@ -13,10 +13,10 @@ export function Stats({stats}:stats){
     // const [hp,setHp] = useState<number>(0)
     console.log(stats)
     return (
-        <div className="">
+        <div>
             {stats?.map(stat => (
-                <div className="flex gap-[20px] mt-[20px] mb-[20px]">
-                    <div className='flex-2 w-[100px]'>
+                <div className="flex gap-[10px] mt-[20px] mb-[20px]">
+                    <div className='flex-2 w-[115px]'>
                         <p>{stat.stat.name}</p>
                     </div>
                     <input type="range" max="200" className="min-w-[200px] flex-1" value={stat.base_stat}/>
