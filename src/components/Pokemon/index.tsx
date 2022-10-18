e62a731763753078ead1418d45ec4e148f717125
Mateus Meneses <mateusppa10@gmail.com>
2022-10-18 19:03:56 -0300

    :lipstick: Resolvendo área em branco fora da div

--- src/components/Pokemon/index.tsx
+++ src/components/Pokemon/index.tsx
@@ -6,7 +6,7 @@ import { Stats } from "./Stats";
 export function Pokemon({name='Informe', types, image, stats}:IPokemon){
     console.log('teste',stats, types)
     return (
-        <div className="flex justify-around gap-[50px] max-w-[100%] m-[50px] mb-[100px]  pt-[100px] pb-[100px] bg-gradient-to-r from-red-500 to-yellow-500  border-8">
+        <div className="flex justify-around gap-[50px] max-w-[100%]">
             <div className="flex flex-col items-center justify-center max-w-[30%] m-[50px]">
                 <img src={image} alt="" className="w-[100%] min-w-[200px]"/> 
             </div>
